8a4600ae3a626a05673780eca730a22519b2297d
Anthony Joyeux <anthony.joyeux@loccitane.com>
2024-07-26 00:01:17 +0200

    add some style

--- src/app.vue
+++ src/app.vue
@@ -3,8 +3,10 @@ import BirthdatePage from './views/birthdate-page.vue'
 </script>
 
 <template>
-    <div class="flex">
+    <div class="flex pl-2 bg-[#EAF0CE]">
         <img alt="logo" src="/logo.svg" />
     </div>
-    <BirthdatePage />
+    <div class="flex items-center justify-center">
+        <BirthdatePage />
+    </div>
 </template>
--- src/views/birthdate-page.vue
+++ src/views/birthdate-page.vue
@@ -12,8 +12,12 @@ const updateResult = (event: Event) => {
 </script>
 
 <template>
-    <span>Date de naissance</span>
-    <input @input="updateResult" type="date" />
-    <div data-testid="first-method-result">{{ firstMethod(result) }}</div>
-    <div data-testid="second-method-result">{{ secondMethod(result) }}</div>
+    <div class="p-4 flex flex-col gap-2 bg-red-500">
+        <div class="flex gap-4">
+            <span>Date de naissance</span>
+            <input @input="updateResult" type="date" />
+        </div>
+        <div data-testid="first-method-result">{{ firstMethod(result) }}</div>
+        <div data-testid="second-method-result">{{ secondMethod(result) }}</div>
+    </div>
 </template>
