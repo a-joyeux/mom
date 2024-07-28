45f24291cce419f4fe5ecc2e501fde613146e06a
Anthony Joyeux <anthony.joyeux@loccitane.com>
2024-07-28 23:49:25 +0200

    fix tests

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -5,11 +5,6 @@ import BirthdatePage from '../src/views/birthdate-page.vue'
 import NamesPage from '../src/views/names-page.vue'
 
 describe('App', () => {
-    it('should display app logo', () => {
-        const wrapper = shallowMount(App)
-
-        expect(wrapper.find('img').attributes().src).toBe('/logo.svg')
-    })
     it('should display birthdate page', () => {
         const wrapper = shallowMount(App)
 
