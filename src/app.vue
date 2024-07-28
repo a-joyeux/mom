a298e9aaf6c63bfada40a6053661e65a8b7e5b82
Anthony Joyeux <anthony.joyeux@loccitane.com>
2024-07-28 23:47:21 +0200

    remove header for now

--- src/app.vue
+++ src/app.vue
@@ -4,8 +4,7 @@ import NamesPage from './views/names-page.vue'
 </script>
 
 <template>
-    <div class="flex pl-2 bg-[#EAF0CE] items-center gap-4 font-bold"><img alt="logo" src="/logo.svg" /> Mom</div>
-    <div class="flex flex-col bg-[#D2FDFF] h-full p-4">
+    <div class="flex flex-col bg-[#D2FDFF] h-full p-4 gap-4">
         <BirthdatePage />
         <NamesPage />
     </div>
